fe3d1db3a137cba89cfafc197d8ec4bcf535929b
lesiya0012 <lesiya0012@gmail.com>
2025-12-14 12:39:26 +0530

    navbar done

--- front/src/MainPage.jsx
+++ front/src/MainPage.jsx
@@ -1,5 +1,5 @@
 // MainPage.jsx
-import Navbar from './components/Navbar.';
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import AboutPage from './pages/AboutPage';
